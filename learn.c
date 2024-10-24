cf970b6ab59a3fa6f2c11080bacc4aac9978fb2d
i9 <oluwarinolasam@gmail.com>
2024-10-24 18:45:32 +0100

    update

--- learn.c
+++ learn.c
@@ -6,12 +6,21 @@ int main() {
   int i = 8;
   double j = 2;
 
-  char* str1 = "How are you? " "I'm fine";
+  char str1[] = "How are you? " "I'm fine";
+  char str2[] = ". Me too!";
 
-  int lenStr = strlen(str1);
+  char str3[strlen(str1)+strlen(str2)];
+  sprintf(str3, "%s%s", str1, str2);
+
+  char lett = '9';
+
+  enum months { JAN = 1, FEB, MAR, APR };
 
   printf("%.3f\n", j / i);
-  printf("%s\n", str1);
+  printf("%s\n", str3);
+  printf("%c\n", lett);
+  printf("%d\n", lett);
+  printf("%d\n", JAN);
 
   return 0;
 }
