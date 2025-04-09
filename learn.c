e7501ba40a913a7517243043811e28881ab429ad
i9 <oluwarinolasam@gmail.com>
2025-04-09 18:54:21 +0100

    update

--- learn.c
+++ learn.c
@@ -1,21 +1,42 @@
 #include <stdio.h>
 #include <string.h>
 
-#define LOWER 0
-#define UPPER  300
-#define STEP 20
+int strindex(const char *source, const char *pattern) {
+  int i, j, k;
+  for (i = 0; source[i] != '\0'; i++) {
+    for (j = i, k = 0; pattern[k] != '\0' && source[j] == pattern[k]; j++, k++);
+    
+    if (pattern[k] == '\0') {
+      return i;
+    }
+  }
+
+  return -1;
+}
+
+int strlastindex(const char *source, const char *pattern) {
+  int i, j, k;
+
+  int lastindex = -1;
+
+  for (i = 0; source[i] != '\0'; i++) {
+    for (j = i, k = 0; pattern[k] != '\0' && source[j] == pattern[k]; j++, k++);
+    
+    if (pattern[k] == '\0') {
+      lastindex = i;
+    }
+  }
+
+  return lastindex;
+}
 
 int main() {
 
-  int c;
+  char *source = "one two three one two three";
+  char *pattern = "four";
 
-  c = getchar();
-  while (c != EOF) {
-    printf("---");
-    putchar(c);
-    printf("+++");
-    c = getchar();
-  }
-  
+  printf("Index of '%s' in '%s' is %d\n", pattern, source, strindex(source, pattern));
+  printf("Last index of '%s' in '%s' is %d\n", pattern, source, strlastindex(source, pattern));
+
   return 0;
 }
