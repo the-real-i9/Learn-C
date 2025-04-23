bd3885822e1e7d65888692aed3bc19926d06607f
i9 <oluwarinolasam@gmail.com>
2025-04-23 19:51:54 +0100

    update

--- learn.c
+++ learn.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
-#include <string.h>
+#include <math.h>
+
+struct point {
+  double x;
+  double y;
+};
+
+double distance(struct point pointA, struct point pointB) {
+  // y / x
+
+  return fabs((pointA.y - pointB.y) / (pointA.x - pointB.x));
+}
 
 int main() {
 
+  struct point pointA;
+
+  printf("PointA (x,y): ");
+  scanf("%lf,%lf", &pointA.x, &pointA.y);
   
+  struct point pointB;
+
+  printf("PointB (x,y): ");
+  scanf("%lf,%lf", &pointB.x, &pointB.y);
+
+  printf("The distance between PointA and PointB is %.1f\n", distance(pointA, pointB));
+
   return 0;
 }
